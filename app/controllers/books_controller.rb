67766ab689bf5d670c59fe55bfd14e5cf5d5233e
EC2 Default User <ec2-user@ip-172-31-34-56.ec2.internal>
2022-01-25 14:55:27 +0000

    [Add]flash,validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,18 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+     redirect_to book_path(@book.id)
+    else
+     render :new
+    end
+
+    flash[:create] = "Book was successfully created"
   end
 
   def index
     @book = Book.new
-
     @books = Book.all
   end
 
@@ -24,11 +25,16 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+     redirect_to book_path(@book.id)
+    else
+     render :edit
+    end
+
+      flash[:update] = "Book was successfully updated"
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
 
-  get 'books/new'
   post '/books' => 'books#create'
 
   get '/books' => 'books#index'
